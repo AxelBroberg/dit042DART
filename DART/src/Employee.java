a8ea364499d46fa5859ac538a178b23ec2ccaaa1
Axel Broberg <axelbroberg01@gmail.com>
2020-09-16 16:28:04 +0200

    Mostly done feature 2

--- DART/src/Employee.java
+++ DART/src/Employee.java
@@ -10,6 +10,7 @@ public class Employee {
     double grossSalary;
     double netSalary;
     String name;
+    int bonus;
     static ArrayList<Employee> employeeArrayList = new ArrayList();
 
     Employee(String name){ this.name = name; }
@@ -77,7 +78,8 @@ public class Employee {
         System.out.print(this.name +  " - " );
         System.out.print(getBirthyear() +  " ( ");
         System.out.print(Year.now().getValue()-getBirthyear() + " ): " );
-        System.out.println(getGrossSalary() + " SEK");
+        System.out.println("Gross salary: " + getGrossSalary() + " SEK");
+        if(getNetSalary()!=0) System.out.println("Net salary: " + getNetSalary() + " SEK");
     }
 
     public static void calcNetSalary(){
@@ -96,19 +98,49 @@ public class Employee {
                 found = true;
                 break;
             }
-            if (!found) {
-                System.out.println("Employee with id " + i + " not found.");
-            }
         }
+        if (!found) { System.out.println("Employee with id " + i + " not found."); }
 
         System.out.println(employeeArrayList.get(i).getNetSalary());
 
         Screens.managerScreen();
     }
 
+    public static void bonus(){
+
+        Scanner input = new Scanner(System.in);
+        int bonus = 0;
+        boolean found = false;
+        int i;
+        System.out.print("Enter ID of employee to see what bonus employee is eligible to: "); int ID = input.nextInt(); //FIX
+
+        for(i = 0; i < employeeArrayList.size(); i++) {
+            if (employeeArrayList.get(i).ID == ID) {
+                if ((Year.now().getValue()-employeeArrayList.get(i).getBirthyear()) < 22) { bonus = 4000; }
+                else if ((Year.now().getValue()-employeeArrayList.get(i).getBirthyear()) < 30) { bonus = 6000; }
+                else { bonus = 7500; }
+                employeeArrayList.get(i).setNetSalary(employeeArrayList.get(i).getNetSalary() + bonus);
+                found = true;
+                break;
+            }
+        }
+        if (!found) { System.out.println("Employee with id " + i + " not found."); }
+
+        System.out.println("Employee bonus is: " + bonus);
+
+        Screens.managerScreen();
+    }
+
     /*
-Employees that receive less than 100,000.00 SEK per year pay no taxes. Their net salary is their full gross salary
-Employees that receive an amount greater than or equal to 100,000.00 SEK per year pay 30% of their gross salary as taxes.
+
+        Employees receive a bonus based on their age.
+        This bonus is not taxable, meaning that is directly added to the monthly net salary of the employee.
+        The bonus and age brackets are:
+
+        Age < 22 years will receive 4000 SEK
+        Between 22 and 30 years will receive 6000 SEK
+        Older than 30 years will receive 7500 SEK
+
      */
 
 }
